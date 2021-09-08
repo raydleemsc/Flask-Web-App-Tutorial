805cd2c3d84f4b06b0bc23e851580f17708438b7
Raymond D. Lee, M. Sc <raydlee.msc@gmail.com>
2021-09-08 16:01:43 +0700

    env_vars

--- wsgi.py
+++ wsgi.py
@@ -1,10 +1,26 @@
 #!/usr/bin/python2.7
-from website import create_app as application
-application.debug = True
-#def application(environ, start_response):
-#    status = '200 OK'
-#    output = str(environ['mod_wsgi.version'])
-#    response_headers = [('Content-type', 'text/plain'),
-#                        ('Content-Length', str(len(output)))]
-#    start_response(status, response_headers)
-#    return [output]
+#from website import create_app as application
+#application.debug = True
+def application(environ, start_response):
+    line_break='<br/>'
+    status = '200 OK'
+    output = '<h1>Environment Versions</h1>'
+    output += line_break
+    output += '<ul>'
+    output += '<li>'
+    output += 'mod_wsgi.version = ' + str(environ['mod_wsgi.version'])
+    output += '</li>'
+    output += line_break
+    output += '<li>'
+    output += env_string(environ, 'mod_wsgi.version')
+    output += '</li>'
+    output += line_break
+    output += '</ul>'
+    response_headers = [('Content-type', 'text/plain'),
+                        ('Content-Length', str(len(output)))]
+    start_response(status, response_headers)
+    return [output]
+
+def env_string(e,env_var):
+    if e.has_key(env_var):
+        return env_var + ' = ' + str(e[env_var])
