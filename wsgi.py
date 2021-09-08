61180658c8a2f199ff454c5b0f9daa9205592c4e
Raymond D. Lee, M. Sc <raydlee.msc@gmail.com>
2021-09-08 12:41:15 +0700

    switch back to app

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,8 @@
-def application(environ, start_response):
-    status = '200 OK'
-    output = str(environ['mod_wsgi.version'])
-    response_headers = [('Content-type', 'text/plain'),
-                        ('Content-Length', str(len(output)))]
-    start_response(status, response_headers)
-    return [output]
-#from website import create_app as application
+from website import create_app as application
+#def application(environ, start_response):
+#    status = '200 OK'
+#    output = str(environ['mod_wsgi.version'])
+#    response_headers = [('Content-type', 'text/plain'),
+#                        ('Content-Length', str(len(output)))]
+#    start_response(status, response_headers)
+#    return [output]
