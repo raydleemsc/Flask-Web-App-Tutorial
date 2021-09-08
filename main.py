ae0d2538a87165b432a3815757503d066ba76d6e
Raymond D. Lee, M. Sc <raydlee.msc@gmail.com>
2021-09-08 10:35:38 +0700

    as application

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from website import create_app
+from website import create_app as application
 
-app = create_app()
+app = application()
 
 if __name__ == '__main__':
     app.run(debug=True,port=5001)
