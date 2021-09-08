8c3a16b56f5f48e2076bfcf9e60280d6200f0bf2
Raymond D. Lee, M. Sc <raydlee.msc@gmail.com>
2021-09-08 16:04:37 +0700

    env_vars html

--- wsgi.py
+++ wsgi.py
@@ -16,7 +16,7 @@ def application(environ, start_response):
     output += '</li>'
     output += line_break
     output += '</ul>'
-    response_headers = [('Content-type', 'text/plain'),
+    response_headers = [('Content-type', 'text/html'),
                         ('Content-Length', str(len(output)))]
     start_response(status, response_headers)
     return [output]
